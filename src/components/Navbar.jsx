9dc000b205d3beba388ca525c0819122f9b005b4
Daniel Guzman Sagredo <danguz80@outlook.com>
2024-08-31 17:52:19 -0400

    2do commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,32 +8,21 @@ const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark text-white bg-dark ">
-      {/* <a className="navbar-brand ms-4" href="#">Pizzeria Mamma Mía!</a> */}
       <Link to="/" className="text-white navbar-brand ms-4 text-decoration-none">Pizzeria Mamma Mía!</Link>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ">
-          {/* <li className="nav-item">
-            <a className="nav-link" href="#"><button type="button" class="btn btn-outline-secondary">🍕 Home</button></a>
-          </li> */}
           {token ? (
             <>
               <li className="nav-item">
-                {/* <a className="nav-link" href="#"><button type="button" class="btn btn-outline-secondary">🔓 Profile</button></a> */}
                 <Link to="/profile" className="text-white ms-3 text-decoration-none">🔓 Logout</Link>
               </li>
-              {/* <li className="nav-item">
-                <a className="nav-link" href="#"><button type="button" class="btn btn-outline-secondary">🔒 Logout</button></a>
-                <Link to="/profile" className="text-white ms-3 text-decoration-none">Home</Link>
-              </li> */}
             </>
           ) : (
             <>
               <li className="nav-item">
-                {/* <a className="nav-link" href="#"><button type="button" class="btn btn-outline-secondary">🔐 Login</button></a> */}
                 <Link to="/login" className="text-white ms-3 text-decoration-none">🔓 Login</Link>
               </li>
               <li className="nav-item">
-                {/* <a className="nav-link" href="#"><button type="button" class="btn btn-outline-secondary">🔐 Register</button></a> */}
                 <Link to="/register" className="text-white ms-3 text-decoration-none">🔓 Register</Link>
               </li>
             </>
@@ -41,7 +30,6 @@ const Navbar = () => {
         </ul>
         <form className="position-absolute end-0 me-4">
             <button className="btn btn-outline-info" type="submit">
-              {/* <a className="nav-link btn-hover" href="#">🛒 Total: ${total.toLocaleString()}</a>*/} 
               <Link to="/cart" className="text-white ms-3 text-decoration-none nav-link btn-hover">🛒 Total: ${total.toLocaleString()}</Link>
             </button>
         </form>
